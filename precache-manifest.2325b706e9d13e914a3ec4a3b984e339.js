d5984795d8e638fdae90f4c1dd68e2d8ea47926f
franco giovannangelo <franco.giovannangelo@ellecktra.com>
2020-06-28 04:25:13 -0300

    Updates

---
+++ precache-manifest.2325b706e9d13e914a3ec4a3b984e339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d65050ee47761ae2a01102156b1323",
+    "url": "/GOworkers-EMTMadrid-page/index.html"
+  },
+  {
+    "revision": "a50276b3ac7d3655e087",
+    "url": "/GOworkers-EMTMadrid-page/static/css/main.7f10303e.chunk.css"
+  },
+  {
+    "revision": "336b798f585d7c99723a",
+    "url": "/GOworkers-EMTMadrid-page/static/js/2.7af6a05e.chunk.js"
+  },
+  {
+    "revision": "8bb9da2e92887b18f123da5413bfe448",
+    "url": "/GOworkers-EMTMadrid-page/static/js/2.7af6a05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50276b3ac7d3655e087",
+    "url": "/GOworkers-EMTMadrid-page/static/js/main.ff5296f9.chunk.js"
+  },
+  {
+    "revision": "a779a7f4399f5928c6df",
+    "url": "/GOworkers-EMTMadrid-page/static/js/runtime-main.10633a40.js"
+  }
+]);
